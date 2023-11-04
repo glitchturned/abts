436e15fb15ad98fcf523d6c8a564b3cb0b283b20
glitchturned <149906810+glitchturned@users.noreply.github.com>
2023-11-04 16:20:44 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,93 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        # . . . #
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.FreeFall, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        # . . . .
+        # . . . .
+        `)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showLeds(`
+        . . . # .
+        . . . # .
+        . # # # .
+        . # . # .
+        . # # # .
+        `)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # # # # .
+        `)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showLeds(`
+        # # . . .
+        # . # . #
+        # # # . #
+        . # # . #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # # # # .
+        # . . # .
+        # # # # .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        . . . . #
+        # # # # #
+        `)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # # # # #
+        # . # . .
+        # . . # .
+        `)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showLeds(`
+        # . . # .
+        # . . # .
+        # . . # .
+        # . . # .
+        # # # # .
+        `)
+})
+basic.forever(function () {
+	
+})
